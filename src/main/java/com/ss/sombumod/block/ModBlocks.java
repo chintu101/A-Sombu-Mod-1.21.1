adc45b785f5ef594f4a33417475f71eea0973766
Sarvesh Sathyanarayanan <sarvesh112170@gmail.com>
2025-06-20 10:49:40 +0530

    created package and deferred register for block

---
+++ src/main/java/com/ss/sombumod/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package com.ss.sombumod.block;
+
+import com.ss.sombumod.SombuMod;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(SombuMod.MODID);
+
+    
+}
